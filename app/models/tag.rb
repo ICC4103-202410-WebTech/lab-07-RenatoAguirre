1a4fb5898acc4cf927154071f257d5e468700757
RenatoAguirre <Raaguirre1@miuandes.cl>
2024-04-25 21:05:12 -0400

    feat/6 added validation to tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ApplicationRecord
   has_many :post_tag
   has_many :posts, through: :post_tag
+
+  validates :name, presence: { message: "Please provide a name for the tag." }, uniqueness: { message: "Tag with this name already exists." }
 end
